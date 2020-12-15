20fc7d6c4265f314fdd5b4fcc640940e3e9e214a
kidaniland <mdcvillas@gmail.com>
2020-12-14 22:40:24 -0300

    familia cuerda agregada al objeto de JSdata

---
+++ src/data/orquesta/orquesta.js
@@ -0,0 +1,49 @@
+export default {
+  "orquesta": [{
+    "nombre": "violín",
+    "clave": "sol",
+    "familia": "cuerda frotada",
+    "cuerdas": "sol, re, la, mi",
+    "sobre": "Instrumento musical de cuerda de unos 60 cm de largo (mástil incluido) formado por una caja de madera hueca y alargada, con un estrechamiento en la parte media y con dos aberturas acústicas en forma de f en el centro, y un brazo (mástil) corto y sin trastes, a lo largo del cual se prolongan cuatro cuerdas y en cuyo extremo se sitúan las clavijas, que permiten afinar el instrumento; se toca colocándola entre el hombro y el mentón y frotando las cuerdas con un arco.",
+    "sonido": "violin.mp3",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "miniatura": "miniVio1.jpg",
+    "posicion": "imagen.png"
+  },
+  {
+    "nombre": "viola",
+    "clave": "do",
+    "familia": "cuerdas frotada",
+    "cuerdas": "do, sol, re, la",
+    "sobre": "La viola es un instrumento similar en cuanto a materiales y construcción al violín pero de mayor tamaño y sonido más grave. Su tesitura se sitúa entre los graves y medios del violín y los agudos del violonchelo.  También es considerada como el contralto o el tenor dramático de la familia de las cuerdas.",
+    "relacionado": "violin, violoncello, contrabajo",
+    "sonido": "violin.mp3",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "miniatura": "miniVio1.jpg",
+    "posicion": "posicion1.jpg"
+  },
+  {
+    "nombre": "violoncello",
+    "clave": "fa",
+    "familia": "cuerdas frotada",
+    "cuerdas": "do, sol, re, la",
+    "sobre": "Es uno de los instrumentos básicos y fundamentales de la orquesta dentro del grupo de las cuerdas, realizando normalmente las partes graves, aunque su versatilidad también permite a los instrumentistas que lo tocan interpretar partes melódicas. El violonchelo también se puede tocar en la salsa. Tradicionalmente está considerado como uno de los instrumentos de cuerda que más se parecen a la voz humana.",
+    "relacionado": "violín, viola, contrabajo",
+    "sonido": "violin.mp3",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "miniatura": "miniVio1.jpg",
+    "posicion": "posicion1.jpg"
+  },
+  {
+    "nombre": "contrabajo",
+    "clave": "fa",
+    "familia": "cuerdas frotada",
+    "cuerdas": "mi, la, re, sol",
+    "sobre": "Su origen se remonta al siglo XVI y fue una evolución de la viola da gamba y del violone bajo. Su gran tamaño, por aquel entonces mayor que el actual, lo dejó al margen del cuarteto de cuerda, formado por dos violines, una viola y un violonchelo. Hay quien afirma que el contrabajo no puede considerarse un verdadero miembro de la familia del violín. Y es que a finales del siglo XV su forma era la del violone a corde, el miembro más grande de la familia de la viola, que tenía unas dieciséis cuerdas.",
+    "relacionado": "viola, violoncello, violín",
+    "sonido": "violin.mp3",
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "miniatura": "miniVio1.jpg",
+    "posicion": "posicion1.jpg"
+  }]
+};
