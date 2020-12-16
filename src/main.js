eb5fbf83b243e21f892e3caada5f05bc1dbf1955
kidaniland <mdcvillas@gmail.com>
2020-12-16 18:30:34 -0300

    HTML y CSS armada estructura para navegador vertical

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/orquesta/orquesta.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
 console.log(example, data);
+
+function toggleMenu(){
+    let navigation = document.querySelector('.navigation');
+    let toggle = document.querySelector('.toggle');
+    navigation.classList.toggle('active');
+    toggle.classList.toggle('active');
+    
+}
