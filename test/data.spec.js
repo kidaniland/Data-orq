80b866e19d6b5cdb722c9c187e3b490cc83a74f1
kidaniland <mdcvillas@gmail.com>
2021-01-31 17:12:10 -0300

    Test iniciado, falta llevarlo al 100%

--- test/data.spec.js
+++ test/data.spec.js
@@ -5,25 +5,16 @@ describe('filter musical instrument by clef', () => {
   it('filterClave is a function', () => {
     expect(typeof filterClave).toBe('function');
   });
-  it('should return array from intrument by clef', () => {
-    expect(filterClave("do")).toBe('array')
-  });
 });
 
 describe('filter musical instrument by family', () => {
   it('filterFamily is a function', () => {
     expect(typeof filterFamily).toBe('function');
   });
-  it('should return array from family', () => {
-    expect(filterFamily("cuerdas")).toBe('array');
-  });
 });
 
 describe('show instrument card', () => {
   it('selectInstrument is a function', () => {
     expect(typeof selectInstrument).toBe('function');
-  });
-  it('returns findInstrument should be a objet', () => {
-    expect(selectInstrument("Piano")).toBe('array');
   });
 });
